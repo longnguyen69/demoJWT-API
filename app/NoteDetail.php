9f583f971df99f306acce6c57e2a8917c18ac97b
longnguyen69 <nguyenthanhlongtlck@gmail.com>
2020-08-05 15:03:56 +0700

    Update#taskRefactor refactor code and add comment function

--- app/NoteDetail.php
+++ app/NoteDetail.php
@@ -8,11 +8,17 @@ class NoteDetail extends Model
 {
     protected $table = 'noteDetails';
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function note()
     {
         return $this->belongsTo(Note::class);
     }
 
+    /**
+     * @param $id_note
+     */
     public function createNoteDetail($id_note)
     {
         $todoDetail = new NoteDetail();
@@ -20,16 +26,27 @@ class NoteDetail extends Model
         $todoDetail->save();
     }
 
+    /**
+     * @param $note_id
+     * @return mixed
+     */
     public function findByNoteId($note_id)
     {
         return NoteDetail::where('note_id', '=', $note_id)->first();
     }
 
+    /**
+     * @param $note_detail
+     */
     public function destroyNoteDetail($note_detail)
     {
         $note_detail->delete();
     }
 
+    /**
+     * @param $note_id
+     * @param $desc
+     */
     public function updateNoteDetail($note_id, $desc)
     {
         $note = $this->findByNoteId($note_id);
